fa0d68e1e3d8b2f11d4fb39dc5327776665ea3c5
macmpaxep <magcanit@gmail.com>
2026-01-12 18:54:32 +0500

    Added CORS Details

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from app.routers import products, cart
 
 app = FastAPI(title="FS Catalog API")
+
+origins = [
+    "http://localhost:3000",       # фронтенд на локальном хосте
+    "https://your-frontend-domain.com"  # или фронтенд на Render/Netlify/Vercel
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       # кто может делать запросы
+    allow_credentials=True,
+    allow_methods=["*"],         # разрешаем GET, POST, PUT, DELETE
+    allow_headers=["*"],
+)
 
 # Подключаем роутеры с префиксом /api
 app.include_router(products.router, prefix="/api")
